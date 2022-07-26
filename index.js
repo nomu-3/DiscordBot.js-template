ae0b0c69a36b54d70443e2d68308e7fcd5e5b128
Nomusan <94737320+nomu-3@users.noreply.github.com>
2022-07-26 10:16:19 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿const Discord = require('discord.js');
+const { Client, MessageAttachment } = require('discord.js');
+const allInvites = {}
+
+const data = new Map()
+const client = new Discord.Client()
+
+ client.on('message', async message => {
+if (message.author.bot) return;
+if (message.content.startsWith('人が打つコマンド') && message.guild) {
+const reply = await message.channel.send('botのメッセージ');
+}
+})
+            client.on('ready', () => {
+                console.log('ログイン成功');
+            });
+client.login('ここにとーくん')
